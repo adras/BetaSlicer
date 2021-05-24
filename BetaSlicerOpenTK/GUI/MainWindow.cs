e4bb4c82a1f736d2dff671e6a068dcdfa54da4d0
Andreas Kurka <andreas.kurka@gmail.com>
2021-05-24 15:25:19 +0200

    Added OpenTK and WPF examples

---
+++ BetaSlicerOpenTK/GUI/MainWindow.cs
@@ -0,0 +1,74 @@
+﻿using OpenTK.Graphics.OpenGL4;
+using OpenTK.Mathematics;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using ImGuiNET;
+
+
+namespace BetaSlicer.GUI
+{
+    class MainWindow : GameWindow
+    {
+        ImGuiController _controller;
+
+        public MainWindow(GameWindowSettings gameWindowSettings, NativeWindowSettings nativeWindowSettings) : base(gameWindowSettings, nativeWindowSettings)
+        {
+        }
+
+        protected override void OnLoad()
+        {
+            base.OnLoad();
+
+            Title += ": OpenGL Version: " + GL.GetString(StringName.Version);
+
+            _controller = new ImGuiController(ClientSize.X, ClientSize.Y);
+        }
+
+        protected override void OnResize(ResizeEventArgs e)
+        {
+            base.OnResize(e);
+
+            // Update the opengl viewport
+            GL.Viewport(0, 0, ClientSize.X, ClientSize.Y);
+
+            // Tell ImGui of the new size
+            _controller.WindowResized(ClientSize.X, ClientSize.Y);
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            base.OnRenderFrame(e);
+
+            _controller.Update(this, (float)e.Time);
+
+            GL.ClearColor(new Color4(0, 32, 48, 255));
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit | ClearBufferMask.StencilBufferBit);
+
+            ImGui.ShowDemoWindow();
+
+            _controller.Render();
+
+            Util.CheckGLError("End of frame");
+
+            SwapBuffers();
+        }
+
+        protected override void OnTextInput(TextInputEventArgs e)
+        {
+            base.OnTextInput(e);
+
+
+            _controller.PressChar((char)e.Unicode);
+        }
+
+        protected override void OnMouseWheel(MouseWheelEventArgs e)
+        {
+            base.OnMouseWheel(e);
+
+            _controller.MouseScroll(e.Offset);
+        }
+    }
+}
---
+++ BetaSlicerOpenTK/Program.cs
@@ -0,0 +1,30 @@
+﻿using BetaSlicer.GUI;
+using OpenTK.Windowing.Desktop;
+using System;
+
+namespace BetaSlicer
+{
+    class Program
+    {
+        const string WINDOW_TITLE = "BetaSlicer V0.01";
+
+        static void Main(string[] args)
+        {
+            GameWindowSettings gameWindowSettings = GameWindowSettings.Default;
+
+            NativeWindowSettings nativeWindowSettings = new NativeWindowSettings()
+            {
+                Size = new OpenTK.Mathematics.Vector2i(800, 600),
+                APIVersion = new Version(4, 5),
+                //StartFocused,
+                //StartVisible,
+                Title = WINDOW_TITLE,
+                //WindowState = OpenTK.Windowing.Common.WindowState.Normal
+            };
+
+            // GameWindowSettings.Default, new NativeWindowSettings() { Size = new Vector2i(1600, 900), APIVersion = new Version(4, 5) 
+            MainWindow window = new MainWindow(gameWindowSettings, nativeWindowSettings);
+            window.Run();
+        }
+    }
+}
